d7b6d725d8bb66fa845aeb2b3bbe634081ba8849
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-03-26 18:35:03 -0400

    add time when click on box

--- lib/mygame.py
+++ lib/mygame.py
@@ -4,10 +4,12 @@ from random import randint
 
 
 def display_score():
-    current_time = current_time = start_time - int(pygame.time.get_ticks() / 1000)
+    current_time = start_time - int(pygame.time.get_ticks() / 1000)
     current_time = max(0, current_time) # Ensure current_time doesn't go below 0
     score_surface = test_font.render(f'Time Alive: {current_time}', False, (64, 64, 64))
     score_rect = score_surface.get_rect(center = (400, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
     screen.blit(score_surface, score_rect)
     return current_time
 
@@ -22,11 +24,13 @@ pygame.display.set_caption('Blade')# it names the game on the window
 clock = pygame.time.Clock() #Clock object keeps track of time and control frame rate. Here it does nothing, it's being called in the while loop
 test_font = pygame.font.Font('lib/font/Pixeltype.ttf', 50) #this works with our variable text_surface. It determines the kind of font, and size
 
-test_surface = pygame.Surface((100, 100)) # tuple is the size of item = 100px by 100px
-test_surface.fill('Red')
-test_rect = test_surface.get_rect(midbottom = (400,250)) #get_rect is rectangle to contain the item so that it can be controlled. x,y axis position: 400px to the left by 200px from the top.
+box_surface = pygame.Surface((100, 100)) # this creates item. tuple is the size of item = 100px by 100px
+box_surface.fill('Red') #gives item color
+box_rect = box_surface.get_rect(midbottom = (400,250)) #get_rect is rectangle to contain the item so that it can be controlled. x,y axis position: 400px to the left by 200px from the top.
 
+box_gravity = 0
 
+add_time = 5
 
 while True:
     screen.fill(('white'))
@@ -35,17 +39,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()# this quits the game
             exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            print('MOUE UP')
 
-    screen.blit(test_surface, test_rect) #blit grabs the rectangle drawn around the surface from (midbottom) point, and depends the point being grabbed, it positions the surface on x,y axis position: 400px to the left by 200px from the top
-    test_rect.x += 5
-    if test_rect.right >= 950: test_rect.left = 0
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if box_rect.collidepoint(event.pos):
+                print('collision')
+                start_time += add_time
+                
+            
+    # box_gravity += 0.005 #this moves the box faster
+    # box_rect.x += box_gravity
+    screen.blit(box_surface, box_rect) #blit grabs the rectangle drawn around the surface from (midbottom) point, and depends the point being grabbed, it positions the surface on x,y axis position: 400px to the left by 200px from the top
+    box_rect.x += 5
+    if box_rect.right >= 950: box_rect.left = 0
+    
 
-    mouse_pos = pygame.mouse.get_pos()
-    if test_rect.collidepoint(mouse_pos):
-        print('collision')
+    # mouse_pos = pygame.mouse.get_pos()
+    # if box_rect.collidepoint(mouse_pos):
+    #     print('collision')
 
-    display_score()
+    time = display_score()
     pygame.display.update()
     clock.tick(60)
